c43e79a09c08cf6ea6df42b355ccdc573f008f83
Andrew <andrewzhlee@gmail.com>
2022-08-13 23:39:41 -0400

    Add event listeners for saving

--- components/ResumeEditor/ResumeEditor.tsx
+++ components/ResumeEditor/ResumeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer, useRef } from "react";
 import { useEffect } from "react";
 import { IFile, ISection } from "../../custom2";
 import useResumeById from "../../hooks/use-resume-by-id";
@@ -53,6 +53,13 @@ const ResumeEditor: React.FC = () => {
 
     const [resume, dispatch] = useReducer(resumeReducer, data);
 
+    const ref = useRef<HTMLBaseElement>(null);
+
+    // Event listener that overrides save shortcut (ctrl+s)
+    // void -> always evaluates to undefined -> type is essentially <undefined | null>.
+    // const [saveListener, setSaveListener] =
+    //     useState<EventListenerOrEventListenerObject | null>(null);
+
     useEffect(() => {
         if (data != null) dispatch({ type: "setResume", payload: data });
     }, [data]);
@@ -61,19 +68,75 @@ const ResumeEditor: React.FC = () => {
         dispatch({ type: "addItem", payload: sectionId });
     };
 
+    // const addSaveEventHandler = (event: SyntheticEvent) => {
+    //     if (saveListener === null) {
+    //         // Listen for ctrl+s key combo -> then save focused resume.
+    //         const listener = event.target?.addEventListener(
+    //             "keydown",
+    //             (event) => {
+    //                 if (
+    //                     event instanceof KeyboardEvent &&
+    //                     event.ctrlKey &&
+    //                     event.key === "s"
+    //                 ) {
+    //                     event.preventDefault();
+    //                     event = event as KeyboardEvent;
+    //                     console.log(resume);
+    //                 }
+    //             }
+    //         ) as unknown as EventListenerOrEventListenerObject;
+    //         console.log(listener);
+    //         setSaveListener(listener);
+    //     }
+    // };
+
+    // const removeSaveEventHandler = (event: SyntheticEvent) => {
+    //     console.log(saveListener);
+    //     if (saveListener !== null) {
+    //         console.log(saveListener);
+    //         event.target.removeEventListener("keydown", saveListener);
+    //         setSaveListener(null);
+    //     }
+    // };
+
+    useEffect(() => {
+        const handleSave = (event: KeyboardEvent) => {
+            console.log("fired");
+            if (event.ctrlKey && event.key === "s") {
+                event.preventDefault();
+                console.log(event);
+            }
+        };
+
+        const target = ref?.current;
+        console.log(target);
+        target?.addEventListener("keydown", handleSave);
+
+        return () => {
+            target?.removeEventListener("keydown", handleSave);
+            console.log("removed");
+        };
+    }, []);
+
     return (
-        <section className={classes.section}>
-            <Toolbox />
-            {resume?.sections.map((section, idx) => (
-                <Section
-                    key={`${section.id}-${idx}`}
-                    id={section.id}
-                    title={section.name}
-                    items={section.items || []}
-                    addItem={addItem}
-                />
-            ))}
-            <AddSection addSection={() => dispatch({ type: "addSection" })} />
+        <section className={classes.section} tabIndex={-1} ref={ref}>
+            {ctx.activeResumeId && (
+                <>
+                    <Toolbox />
+                    {resume?.sections.map((section, idx) => (
+                        <Section
+                            key={`${section.id}-${idx}`}
+                            id={section.id}
+                            title={section.name}
+                            items={section.items || []}
+                            addItem={addItem}
+                        />
+                    ))}
+                    <AddSection
+                        addSection={() => dispatch({ type: "addSection" })}
+                    />
+                </>
+            )}
         </section>
     );
 };
