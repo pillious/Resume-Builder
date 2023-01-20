b41875da5d0dfa439f3545458b9313ed7fd0c008
Andrew <andrewzhlee@gmail.com>
2023-01-20 16:19:03 -0500

    font

--- components/UI/Theme.tsx
+++ components/UI/Theme.tsx
@@ -42,18 +42,18 @@ const theme = createTheme({
                     font-family: SegoeUI;
                     src:
                         local("Segoe UI"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/normal/latest.woff2) format("woff2"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/normal/latest.woff) format("woff"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/normal/latest.ttf) format("truetype");
+                        url(/fonts/Segoe-UI/segoe-ui-normal.woff2) format("woff2"),
+                        url(/fonts/Segoe-UI/segoe-ui-normal.woff) format("woff"),
+                        url(/fonts/Segoe-UI/segoe-ui-normal.ttf) format("truetype");
                     font-weight: 400;
                 },
                 @font-face {
                     font-family: SegoeUI;
                     src:
                         local("Segoe UI Semibold"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/semibold/latest.woff2) format("woff2"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/semibold/latest.woff) format("woff"),
-                        url(//c.s-microsoft.com/static/fonts/segoe-ui/west-european/semibold/latest.ttf) format("truetype");
+                        url(/fonts/Segoe-UI/segoe-ui-semibold.woff2) format("woff2"),
+                        url(/fonts/Segoe-UI/segoe-ui-semibold.woff) format("woff"),
+                        url(/fonts/Segoe-UI/segoe-ui-semibold.ttf) format("truetype");
                     font-weight: 600;
                 }
         `,
